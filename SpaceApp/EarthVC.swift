e47acc1813db6a49361407a84d60e21d0ac50ea5
sivam25 <sivam@Sivams-iMac.local>
2017-09-23 12:31:51 -0400

    adding folders

---
+++ SpaceApp/EarthVC.swift
@@ -0,0 +1,39 @@
+//
+//  EarthVC.swift
+//  SpaceApp
+//
+//  Created by Sivam on 2017-09-14.
+//  Copyright © 2017 Sivam. All rights reserved.
+//
+
+import UIKit
+
+class EarthVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func VenusBckBtn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+
+    }
+
+}
